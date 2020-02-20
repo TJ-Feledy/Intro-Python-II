d5a01bad8e3afd2c475f6c4e2a4b07484f90a77a
teej387 <teej387@yahoo.com>
2020-02-19 20:42:27 -0600

    Reformatted Room class

--- src/room.py
+++ src/room.py
@@ -2,16 +2,12 @@
 # description attributes.
 
 class Room:
-  n_to = ''
-  s_to = ''
-  e_to = ''
-  w_to = ''
 
   def __init__(self, name, description):
     self.name = name
     self.description = description
+    self.n_to = None
+    self.s_to = None
+    self.e_to = None
+    self.w_to = None
     
-  # def __str__(self):
-  #   for i in self.name:
-  #     output = f'\n{i}'
-  #   return output
