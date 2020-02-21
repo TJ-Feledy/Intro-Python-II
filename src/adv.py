eb3c6460d064a375497e04afdab38fe72604453b
teej387 <teej387@yahoo.com>
2020-02-20 20:36:32 -0600

    Changed some text and added comments

--- src/adv.py
+++ src/adv.py
@@ -9,7 +9,7 @@ import asyncio
 # Declare all the rooms
 
 lantern = Item('lantern', 'It\'s an old gas lantern.')
-lighter = Item('lighter', 'Oh... How did this get here?')
+lighter = Item('lighter', 'Nice! A Zippo.')
 
 room = {
     'outside':  Room("Outside Cave Entrance",
@@ -61,6 +61,7 @@ player = Player('Add Venturer', location)
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+player.name = input('\nWelcome Adventurer! What is your name? --->  ')
 
 while player.current_room:
   room_items = f'Room Items: '
@@ -71,12 +72,11 @@ while player.current_room:
   for i in player.items:
     your_items += f' -{i.name}- '
 
-  player.name = input('\nWelcome Adventurer! What is your name? --->  ')
 
   print(f'\n-----------------------------------------------------------\n---{player.current_room.name}---')
   print(f'{textwrap.fill(player.current_room.description, 40)}')
   print(f'\n{room_items}')
-  print(your_items)
+  print(f'\n{your_items}')
   user_input = input("""   
                                                                   To move:              'n' for North
   Enter 'take item_name' to take/get an item from the room,                'w' for West        +      'e' for East
@@ -88,50 +88,53 @@ What would you like to do? (enter 'q' to quit) ---> """).lower()
   room_items_names = [i.name for i in player.current_room.items]
   player_items_names = [i.name for i in player.items]
 
+# handle quitting and movement requirements
   if len(user_input.split()) == 1:
-    if 'lantern' not in player_items_names and 'lighter' not in player_items_names:
-      print('\nIt is way too dark in there.  If only there was a way to create enough light to see...')
+    if user_input == 'q':
+      print(f'\nGood bye {player.name}, until next time!')      
+      break
+    elif 'lantern' not in player_items_names and 'lighter' not in player_items_names:
+      print('\nIt is way too dark in there.\nIf only there was a way to create enough light to see...')
     elif 'lantern' in player_items_names and 'lighter' not in player_items_names:
-      print('\nYou have a lantern and the gas is full, but there must be something else you need.')
+      print('\nYou have a lantern and the gas is full,\nbut there must be something else you need.')
     elif 'lantern' not in player_items_names and 'lighter' in player_items_names:
-      print('\nYou have a lighter, but that won\'t be enough light by it\'s self. There must be something else you need.')
+      print('\nYou have a lighter, but that won\'t be enough light by it\'s self.\nThere must be something else you need.')
     else:
-      
+
+# handle movements
       if user_input == 'n':
         if player.current_room.n_to:
           player.current_room = player.current_room.n_to
-          print(f'You have moved North to {player.current_room.name}')
+          print(f'\nYou have moved North to the {player.current_room.name}')
         else:
           print('\nThere is nothing in this direction!')      
           continue
       elif user_input == 's':
         if player.current_room.s_to:
           player.current_room = player.current_room.s_to
-          print(f'You have moved South to {player.current_room.name}')
+          print(f'\nYou have moved South to the {player.current_room.name}')
         else:
           print('\nThere is nothing in this direction!')      
           continue
       elif user_input == 'e':
         if player.current_room.e_to:
           player.current_room = player.current_room.e_to
-          print(f'You have moved East to {player.current_room.name}')
+          print(f'\nYou have moved East to the {player.current_room.name}')
         else:
           print('\nThere is nothing in this direction!')      
           continue
       elif user_input == 'w':
         if player.current_room.w_to:
           player.current_room = player.current_room.w_to
-          print(f'You have moved West to {player.current_room.name}')
+          print(f'\nYou have moved West to the {player.current_room.name}')
         else:
           print('\nThere is nothing in this direction!')      
           continue
-      elif user_input == 'q':
-        print(f'\nGood bye {player.name}, until next time!')      
-        break
       else:
         print(f'\nYou entered {user_input}, that does nothing. Sorry')      
         continue
 
+# handle actions
   elif len(user_input.split()) == 2:
     picked_item = user_input.split()[1]
 
